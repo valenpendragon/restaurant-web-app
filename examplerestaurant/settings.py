3528aaf1e5764aa5fea7d78e7c0708b5374f35c5
Jeffrey Scott <valenpendragon@gmail.com>
2023-07-15 10:17:20 -0400

    Commit database migrations to database. Add database model for items table. Make temporary django secret key more secure.

--- examplerestaurant/settings.py
+++ examplerestaurant/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#%s03s=)5%%1#+d1qu3*s2m6)!o31lssa9am#(#%!9(6ezy@bl"
+SECRET_KEY = os.getenv("CURRENT_DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
