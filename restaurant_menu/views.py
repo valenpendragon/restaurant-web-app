e0b75381d77617ae5d1fc2b84c77b656927e2af7
Jeffrey Scott <valenpendragon@gmail.com>
2023-07-16 09:09:26 -0400

    Complete MenuList view class using django.views.generic.ListView.

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import Item
 
 
 class MenuList(generic.ListView):
-    pass
+    queryset = Item.object.order_by("date_created")
+    template_name = "index.html"
 
 
 class MenuItemDetail(generic.DetailView):
