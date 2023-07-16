4e6e006866c4425cf911a80599c051625e81bc65
Jeffrey Scott <valenpendragon@gmail.com>
2023-07-16 10:42:02 -0400

    Overwrite MenuList.get_context_data method to provide some fake data to build the front end Jinja code for the home page.

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -7,6 +7,10 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("date_created")
     template_name = "index.html"
 
+    def get_context_data(self):
+        context = {"meals": "Pizza"}
+        return context
+
 
 class MenuItemDetail(generic.DetailView):
     model = Item
